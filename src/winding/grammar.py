2a8d372763c06687450f8aa93bac7d39c1bebd1a
Wind <wind@wind.kids>
2025-04-25 23:17:27 -0700

    Update markdown rule to allow NEWLINE and identifier negation in grammar definition

--- src/winding/grammar.py
+++ src/winding/grammar.py
@@ -8,13 +8,13 @@ inline_winding: "@" IDENTIFIER ":" attributes NEWLINE content?
 
 content: (winding | markdown)+
 
-markdown: (image | TEXT_LINE)+
+markdown: (image | TEXT_LINE | NEWLINE)+
 
 attributes: (IDENTIFIER ("," IDENTIFIER)*)?
 
 image: "![" IDENTIFIER "]" "(" URI ")"
 
-IDENTIFIER: /[A-Za-z][A-Za-z0-9_.-]*/
+IDENTIFIER: /!?[A-Za-z][A-Za-z0-9_.-]*/
 URI: /[^\)\n]+/
 TEXT_LINE: /[^\n@!-][^\n]*/
 
