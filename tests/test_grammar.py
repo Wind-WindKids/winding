9c4640879d4d2f69f29dad6d3a1e31d8d05cfe91
Wind <wind@wind.kids>
2025-05-24 13:30:26 -0700

    Bugfix in grammar parsing and update tests for Winding AST structure

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 from winding import grammar
+from pprint import pprint
+
 
 class TestGrammar(unittest.TestCase):
     
@@ -47,7 +49,7 @@ Up with a curl
         try:
             from winding.parser import Lark_StandAlone
             from winding.transformer import WindingTransformer
-            from winding.ast import Winding
+            from winding.ast import Winding, Markdown, Image
         except ImportError:
             self.skipTest("Required modules are not installed")
 
@@ -80,11 +82,60 @@ theme: colorful
 --
 Text
 
+@right.bottom: small, cursive, !black
+
 """
         tree = parser.parse(sample)
         ast = WindingTransformer().transform(tree)
         self.assertIsInstance(ast, Winding)
-        print(ast)
+        pprint(ast)
+
+        expected = Winding(at='this',
+        attributes=[],
+        content=[Winding(at='book',
+                         attributes=['portrait-oriented'],
+                         content=[Winding(at='filename',
+                                          attributes=['test.md'],
+                                          content=[]),
+                                  Markdown(content='The book\n\n')]),
+                 Winding(at='front-cover',
+                         attributes=['portrait-oriented'],
+                         content=[Markdown(content='Text\n\n'),
+                                  Winding(at='top',
+                                          attributes=['large',
+                                                      'landscape-oriented'],
+                                          content=[Markdown(content=Image(caption='caption',
+                                                                          url='http://url')),
+                                                   Markdown(content='\n\n'),
+                                                   Markdown(content=Image(caption='Another '
+                                                                                  'caption',
+                                                                          url='another.png')),
+                                                   Markdown(content='\n\n'),
+                                                   Markdown(content=Image(caption='Yet '
+                                                                                  'another',
+                                                                          url='')),
+                                                   Markdown(content='\n\n'),
+                                                   Markdown(content=Image(caption='',
+                                                                          url='other.png')),
+                                                   Markdown(content='\n\n'),
+                                                   Markdown(content=Image(caption='',
+                                                                          url='')),
+                                                   Markdown(content='\n\n')])]),
+                 Winding(at='spread',
+                         attributes=['landscape-oriented'],
+                         content=[Winding(at='theme',
+                                          attributes=['colorful'],
+                                          content=[]),
+                                  Markdown(content='Text\n\n'),
+                                  Winding(at='right.bottom',
+                                          attributes=['small',
+                                                      'cursive',
+                                                      '!black'],
+                                          content=[Markdown(content='\n')])])])
+        
+        self.assertEqual(ast, expected)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
